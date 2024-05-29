6dbc804a87520acfb4c4d2b5398fedcf3afa1430
AidenInglis <aiden-inglis@live.nmit.ac.nz>
2024-05-29 13:55:09 +1200

    connection.js setup

---
+++ server/db/connection.js
@@ -0,0 +1,18 @@
+//Import MongoClient from mongodb - Class
+import {MongoClient} from 'mongodb';
+
+//Connection Setup
+const connectionString = process.env.ATLAS_URI || "";//Sets the connection string to the value of the MONGODB_URI env var.
+
+//Creating MongoCLient
+const client = new MongoClient(connectionString)//Creating a new instance of MongoClient class. used to connect to mongodb server.
+
+//Connect to the database - MongoDB
+
+let connection;
+
+try {
+    connection = await client.connect();//Connects to the MongoDB server and is asynchronous.
+} catch (e) {
+    console.error(e);//Logs each error to the console.
+}
